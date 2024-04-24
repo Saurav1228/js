f0dca44bca6c1a33c1460a004dcdc348b2a0f99c
Saurav <negisaurav3010@gmail.com>
2024-04-24 17:12:55 +0530

    commit 45

---
+++ main.js
@@ -0,0 +1,17 @@
+// const s = ('mydeveloper');
+// x = s.__proto__;
+// x = s.toUpperCase();
+// x = s.lastIndexOf('m');
+// x = s.__proto__;
+// x = s.substring(0,1).toUpperCase() + s.slice(1);
+
+// console.log(x);
+
+
+//Floor and ceil function
+// x = Math.floor(Math.random()*100) + 1;
+// y = parseInt((Math.floor(Math.random()*100) + 1)/2);
+
+// let sumOutput = 'Sum of x and y is ' + x + y;
+// console.log(sumOutput);
+
